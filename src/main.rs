c0b70c320cd180911105ae5e21281304a201fbe1
mogi <git@paramogi.world>
2026-09-19 18:23:50 +0100

    Update main.rs to open bare repos

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use git2::Repository;
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let repo_path = env::args().nth(1).unwrap_or_else(|| {
+        eprintln!("provide the repo path");
+        process::exit(1);
+    });
+
+    let repo = Repository::open_bare(repo_path).unwrap_or_else(|_| {
+        eprintln!("repo must be bare");
+        process::exit(1);
+    });
 }
