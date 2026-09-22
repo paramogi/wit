49fc156a7e3f8406013ab5d372b723fa49bf6386
mogi <git@paramogi.world>
2026-09-23 00:38:07 +0100

    Make handle_repo return a String with the commit information and handle its Result in main

--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,31 @@ fn main() {
         process::exit(1);
     };
 
-    if let Err(e) = handle_repo(&arg) {
-        eprintln!("{}", e.message());
-        process::exit(1);
-    }
+    let commit = match handle_repo(&arg) {
+        Ok(s) => s,
+        Err(e) => {
+            eprintln!("{}", e.message());
+            process::exit(1);
+        }
+    };
+    println!("{commit}");
 }
 
-fn handle_repo(repo_path: &str) -> Result<(), git2::Error> {
+fn handle_repo(repo_path: &str) -> Result<String, git2::Error> {
     let repo = Repository::open_bare(repo_path)?;
     let mut rev = repo.revwalk()?;
     rev.push_head()?;
+    let mut commit_string = String::new();
     for r in rev {
         let oid = r?;
         let commit = repo.find_commit(oid)?;
-        println!("commit info");
-        println!("-----------");
+        // println!("commit info");
+        // println!("-----------");
         // println!("msg=\"{}\"", commit.message()?.trim());
-        println!("summary=\"{}\"", commit.summary()?.unwrap_or_default());
-        println!("author=\"{}\"", commit.author());
+        // println!("summary=\"{}\"", commit.summary()?.unwrap_or_default());
+        // println!("author=\"{}\"", commit.author());
+        commit_string.push_str(commit.summary()?.unwrap_or_default());
+        commit_string.push_str(&commit.author().to_string());
 
         // TODO : Add date
         let commit_time = commit.time();
@@ -33,9 +40,11 @@ fn handle_repo(repo_path: &str) -> Result<(), git2::Error> {
         let commit_time_seconds = (commit_time.seconds() + commit_timezone as i64 * 60) % 86400;
         let hour = commit_time_seconds / 3600;
         let minutes = (commit_time_seconds - hour * 3600) / 60;
-        println!("time={:02}:{:02}", hour, minutes);
-        println!("-----------\n");
+
+        commit_string.push_str(&format!(" {:02}:{:02}\n", hour, minutes).to_string());
+        // println!("time={:02}:{:02}", hour, minutes);
+        // println!("-----------\n");
     }
 
-    Ok(())
+    Ok(commit_string)
 }
